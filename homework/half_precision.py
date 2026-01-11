7f0ae5a1f9a2548daf541a4301928c612963f1dd
ejc129 <ejc3333@eid.utexas.edu>
2026-01-11 20:19:09 +0000

    half precision work pt1

--- homework/half_precision.py
+++ homework/half_precision.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 import torch
+from torch._C import float32
 
 from .bignet import BIGNET_DIM, LayerNorm  # noqa: F401
-
+import torch.nn.functional as F
 
 class HalfLinear(torch.nn.Linear):
     def __init__(
@@ -17,16 +18,34 @@ class HalfLinear(torch.nn.Linear):
         Feel free to use the torch.nn.Linear class as a parent class (it makes load_state_dict easier, names match).
         Feel free to set self.requires_grad_ to False, we will not backpropagate through this layer.
         """
-        # TODO: Implement me
-        raise NotImplementedError()
+
+
+
+        super().__init__(in_features= in_features, out_features= out_features, bias= bias)
+        self.requires_grad_(False)
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Hint: Use the .to method to cast a tensor to a different dtype (i.e. torch.float16 or x.dtype)
         # The input and output should be of x.dtype = torch.float32
         # TODO: Implement me
-        raise NotImplementedError()
+        if x.dtype != torch.float32:
+          x = x.to(dtype= torch.float32)
 
-
+          # we cast to fp16
+          x_half = x.to(dtype= torch.float16)
+          # this self.weight is here because we have nn.Linear parent
+          weight_half = self.weight.to(dtype= torch.float16, device= x.device)
+          # this self.bias is here because we have nn.Linear parent
+          if self.bias:
+            bias_half = self.bias.to(dtype= torch.float16, device= x.device)
+          
+          # functions as a stateless nn.Linear, so we don't have to store the weight and bias
+          # saves on the memory that is so precious <3 
+          y_half = F.linear(x_half, weight_half, bias_half)
+          y = y_half.to(dtype= torch.float32)
+          return y
+          
 class HalfBigNet(torch.nn.Module):
     """
     A BigNet where all weights are in half precision. Make sure that the normalization uses full
